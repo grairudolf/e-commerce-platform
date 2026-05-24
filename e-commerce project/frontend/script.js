0cddab96d53bc6a1730d4e45bcce404a9d48a979
SAFARI OUSMAN <safariousman02@gmail.com>
2026-05-24 10:58:47 +0100

    inputing and checking for data in the login page

---
+++ e-commerce project/frontend/script.js
@@ -0,0 +1,11 @@
+function handlelogin() {
+    let username = document.getElementById('username').value;
+    let password = document.getElementById('password').value;
+
+    if (username === "" || password === "") {
+        alert("Please enter both user and password!");
+    }
+    else {
+        alert('Welcome back! ${username')
+    }
+}
